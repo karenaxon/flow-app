8ab7d008374275859bc0cf1b8436e5b15d52c3fc
Kaxon <karenaxon@users.noreply.github.com>
2022-05-25 17:06:42 -0700

    Add mutate the blockchain with a profile name

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,101 @@ import * as fcl from "@onflow/fcl";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Home() {
-  const [user, setUser] = useState({ loggedIn: null });
-  const [name, setName] = useState("");
 
-  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+  const [user, setUser] = useState({loggedIn: null})
+  const [name, setName] = useState('')
+  const [transactionStatus, setTransactionStatus] = useState(null)
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), [])
 
   const sendQuery = async () => {
     const profile = await fcl.query({
       cadence: `
-      import Profile from 0xProfile
-      
-      pub fun main(address: Address): Profile.ReadOnly? {
-        return Profile.read(address)
-      }
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
       `,
-      args: (arg, t) => [arg(user.addr, t.Address)],
-    });
-    setName(profile?.name ?? "No Profile");
-  };
+      args: (arg, t) => [arg(user.addr, t.Address)]
+    })
+
+    setName(profile?.name ?? 'No Profile')
+  }
+
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    const transaction = await fcl.tx(transactionId).onceSealed()
+    console.log(transaction)
+  }
+
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg, t) => [arg("Flow Developer!", t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
+  }
 
   const AuthedState = () => {
     return (
       <div className="container">
         <div>Address: {user?.addr ?? "No Address"}</div>
-        <div className="my-2">Profile Name: {name ?? "--"}</div>
-        <button className="btn btn-dark" onClick={sendQuery}>
-          Get Profile Name
-        </button>
-        <button className="btn btn-dark mx-2" onClick={fcl.unauthenticate}>
-          Log Out
-        </button>
+        <div>Profile Name: {name ?? "--"}</div>
+        <div className="mb-2">Transaction Status: {transactionStatus ?? "--"}</div> 
+        <button className="bnt btn-primary mx-1" onClick={sendQuery}>Send Query</button>
+        <button className="bnt btn-primary mx-1" onClick={initAccount}>Init Account</button>
+        <button className="bnt btn-primary mx-1" onClick={executeTransaction}>Execute Transaction</button> 
+        <button className="bnt btn-primary mx-1" onClick={fcl.unauthenticate}>Log Out</button>
       </div>
-    );
-  };
+    )
+  }
 
   const UnauthenticatedState = () => {
     return (
-      <div className="container">
-        <button className="btn btn-dark" onClick={fcl.logIn}>
-          Log In
-        </button>
-        <button className="btn btn-dark mx-2" onClick={fcl.signUp}>
-          Sign Up
-        </button>
+      <div>
+        <button className="bnt btn-primary mx-1" onClick={fcl.logIn}>Log In</button>
+        <button className="bnt btn-primary mx-1" onClick={fcl.signUp}>Sign Up</button>
       </div>
-    );
-  };
+    )
+  }
 
   return (
     <div className="container">
@@ -60,7 +109,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <h1>Flow App</h1>
-      {user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
+      {user.loggedIn
+        ? <AuthedState />
+        : <UnauthenticatedState />
+      }
     </div>
-  );
+  )
 }
